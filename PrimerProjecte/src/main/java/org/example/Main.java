08e47f1c29b21d656530c5acb47b444ed7479bb0
Pol Tordera <poltordera@iesebre.com>
2023-09-19 13:27:51 +0200

    He afegit el primer projecte Java del curs

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Vaig a mostar per la pantalla el missatge "Hello world!"
+        //Shortcut del System.out.println --> sout
+        //println --> ln --> line --> Imprimeix el text i un "enter"
+        //print --> Imprimeix el text sense "enter"
+        System.out.println("Hello world!");
+        System.out.println("Segona linia de text");
+        System.out.print("Me dic Pol");
+        System.out.println(" i soc l'alumne de l'MP03");
+    }
+}
+e
+//Exemple de comentari d'una sola linia
+
+/*Exemple de
+    comentari de
+        més d'una linia
+        */
